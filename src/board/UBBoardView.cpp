20683b8b1f4d37d98c53090ecbc680a3c4295882
Gavin Smith <gavinsmith0123@gmail.com>
2022-10-09 18:13:08 +0100

    larger text boxes

--- src/board/UBBoardView.cpp
+++ src/board/UBBoardView.cpp
@@ -1351,7 +1351,10 @@ void UBBoardView::mouseReleaseEvent (QMouseEvent *event)
 
                 textItem->setTextInteractionFlags(Qt::TextEditorInteraction);
                 textItem->setSelected (true);
-                textItem->setTextWidth(0);
+                if (rubberRect.width() > 100)
+                    textItem->setTextWidth(rubberRect.width());
+                else
+                    textItem->setTextWidth(1200);
                 textItem->setFocus();
             }
         }
